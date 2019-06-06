f28f5f0013d66a6346b2cd7970dde4f1e2abdf65
jeffvswanson <jeff.v.swanson@gmail.com>
2019-06-05 20:27:54 -0600

    Add working heapsort

--- Stanford/10_HeapSort/heap_sort.py
+++ Stanford/10_HeapSort/heap_sort.py
@@ -8,16 +8,21 @@ value from the heap.
 
 def main():
     A = [11, 13, 9, 4, 12, 9, 4, 8, 4]
+    print("Original list: {}".format(A))
 
     # Create the heap
     heap = heapify(A)
 
+    print("List after heapifying: {}".format(heap))
+
+    sorted_list = []
     # Sort the original list
-    for idx, _ in enumerate(A):
-        A[idx] = extract_min(heap)
+    while len(heap) > 1:
+        min_value, heap = extract_min(heap)
+        sorted_list.append(min_value)
 
     print("The sorted list using heapsort is:")
-    print(A)
+    print(sorted_list)
 
 def heapify(A):
     """ Turns the input array into an array resembling a heap."""
@@ -28,7 +33,6 @@ def heapify(A):
 
     for key in A:
         insert(key, heap)
-
 
     return heap
 
@@ -42,11 +46,11 @@ def insert(key, heap):
 
     heap.append(key)
     
-    bubble_up(len(heap), heap)
+    bubble_up(len(heap)-1, heap)
     
     return
 
-def get_parent(child_index):
+def get_parent_index(child_index):
     """
     Get the index of the given key's parent given the index 
     of the child key.
@@ -58,41 +62,43 @@ def get_parent(child_index):
         return 0
     return child_index // 2
 
-def get_left_child(parent_index, heap):
+def get_left_child_index(parent_index, heap):
     """
     Get the index of the left child given the 
     parent node's index.
     """
 
     # Remember, this is a 1-based index.
-    if parent_index * 2 - 1 > len(heap):
+    if parent_index * 2 >= len(heap):
         # There is no left child
         return 0
 
-    return parent_index * 2 - 1
+    return parent_index * 2
 
-def get_right_child(parent_index, heap):
+def get_right_child_index(parent_index, heap):
     """
     Get the index of the right child given the
     parent node's index.
     """
 
     # Remember, this is a 1-based index.
-    if parent_index * 2 > len(heap):
+    if parent_index * 2 + 1 >= len(heap):
         # There is no right child
         return 0
     
-    return parent_index * 2
+    return parent_index * 2 + 1
 
 def bubble_up(child_index, heap):
-    """Push a child node up through the heap to heap property."""
+    """
+    Push a child node up through the heap to maintain heap property.
+    """
 
-    parent_index = get_parent(child_index)
+    parent_index = get_parent_index(child_index)
     # The node in question is the root node
     if parent_index == 0:
         return
 
-    if heap[child_index] > heap[parent_index]:
+    if heap[child_index] < heap[parent_index]:
         heap[child_index], heap[parent_index] = heap[parent_index], heap[child_index]
         bubble_up(parent_index, heap)
     
@@ -101,7 +107,45 @@ def bubble_up(child_index, heap):
 def extract_min(heap):
     """Returns the minimum value of the heap, the root."""
 
-    return heap[1]
+    # Check if the heap only has the 0-element
+    if len(heap) < 2:
+        return heap[0], heap
+
+    root = heap[1]
+    # Swap the last index position into the root node
+    heap[1] = heap[-1]
+    # Pare down the list since the last node became the root
+    del heap[-1]
+    
+    if len(heap) > 1:
+        bubble_down(1, heap)
+
+    return root, heap
+
+def bubble_down(parent_index, heap):
+    """Moves a misplaced node into its appropriate position."""
+    
+    min_val = heap[parent_index]
+    min_index = get_left_child_index(parent_index, heap)
+    
+    # The parent node has no children
+    if min_index == 0:
+        return
+
+    # Find the smaller of the two children
+    right_child_index = get_right_child_index(parent_index, heap)
+    if right_child_index == 0:
+        right_child_index = min_index
+
+    if heap[right_child_index] < heap[min_index]:
+        min_index = right_child_index
+
+    if heap[min_index] < min_val:
+        min_val = heap[min_index]
+
+    if min_val != heap[parent_index]:
+        heap[parent_index], heap[min_index] = heap[min_index], heap[parent_index]
+        bubble_down(min_index, heap)
 
 if __name__ == "__main__":
     main()
--- Stanford/10_HeapSort/test_heap_sort.py
+++ Stanford/10_HeapSort/test_heap_sort.py
@@ -1,24 +1,170 @@
 # test_heap_sort.py
 
+import copy
 import heap_sort as hs
 import unittest
 
-class TestHS(unittest.TestCase):
-    def test_parent(self):
-        
+class TestHeapSort(unittest.TestCase):
+    def test_get_parent_index(self):
+        """
+        Test to ensure proper parent index is returned.
+        """
+
+        # Remember the 0 is a place-holder and heap has a 1-based index.
+        # heap = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
         expected = [0, 1, 4]
-        # Remember heapsort is a 1-based index
-        # The 0 is a placeholder.
-        # test_heap = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-        children = [1, 3, 8]
-        
+        find_parent = [1, 3, 8]
+
         got = []
-        for _, child_idx in children:
-            got.append(hs.get_parent(child_idx))
+        for child in find_parent:
+            got.append(hs.get_parent_index(child))
 
         self.assertEqual(got, expected, 
-        "Incorrect parent returned in get_parent.")
+        "get_parent_index did not return the correct indices.\
+            \n\tExpected: {}, Got: {}".format(expected, got))
 
+    def test_get_left_child_index(self):
+        """
+        Test to ensure proper left_child_index is returned.
+        """
+
+        # Remember, 0 is a place-holder and heap has a 1-based index.
+        heap = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+        expected_indices = [2, 6, 8, 0]
+        parent_indices = [1, 3, 4, len(heap)-1]
+
+        got = []
+        for index in parent_indices:
+            got.append(hs.get_left_child_index(index, heap))
+
+        self.assertEqual(got, expected_indices,
+        "get_left_child_index did not return the correct indices.\
+            \n\tExpected: {}, Got: {}".format(expected_indices, got))
+
+    def test_get_right_child_index(self):
+        """
+        Test to ensure proper right_child_index is returned.
+        """
+
+        # Remember, 0 is a place-holder and heap has a 1-based index.
+        heap = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+        expected_indices = [3, 7, 0, 0]
+        parent_indices = [1, 3, 4, 8]
+
+        got = []
+        for index in parent_indices:
+            got.append(hs.get_right_child_index(index, heap))
+
+        self.assertEqual(got, expected_indices, 
+        "get_right_child_index did not return the correct indices.\
+            \n\tExpected: {}, Got: {}".format(expected_indices, got))
+
+    def test_bubble_up(self):
+        """
+        Test to ensure bubble_up functions appropriately.
+        """
+
+        # Remember, 0 is a place-holder and heap has a 1-based index.
+        original_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        expected = [0, 1, 1, 3, 2, 5, 6, 7, 8, 4]
+
+        heap = copy.deepcopy(original_list)
+        # Check against the root node, nothing should change.
+        hs.bubble_up(1, heap)
+        self.assertEqual(heap, original_list,
+        "bubble_up() failed. The root node should not bubble-up past index 1.\
+            \n\tExpected: {}, Got: {}".format(original_list, heap))
+
+        # Check against the value violating the heap property.
+        heap.append(1)
+        hs.bubble_up(len(heap)-1, heap)
+        self.assertEqual(heap, expected,
+        "bubble_up() failed.\
+            \n\tExpected:\t{}\
+            \n\tGot:\t\t{}".format(expected, heap))
+
+    def test_insert(self):
+        """
+        Test to ensure inserting a key maintains the heap property.
+        """
+
+        # Remember, 0 is a place-holder and heap has a 1-based index.
+        heap = [0]
+        expected = [0, 1]
+
+        hs.insert(1, heap)
+        self.assertEqual(heap, expected, 
+        "insert() failed. Expected: {}, Got {}".format(expected, heap))
+
+        add = [4, 4, 8, 13, 5, 9, 3]
+        expected = [0, 1, 3, 4, 4, 13, 5, 9, 8]
+        for node in add:
+            hs.insert(node, heap)
+
+        self.assertEqual(heap, expected,
+        "insert() failed.\
+            \n\tExpected:\t{}\
+            \n\tGot:\t\t{}".format(expected, heap))
+
+    def test_heapify(self):
+        """
+        Test to ensure validity of heapify function.
+        """
+
+        unheaped = [11, 13, 9, 4, 12, 9, 4, 8, 4]
+        # Remember, 0 is a place-holder and heap has a 1-based index.
+        expected = [0, 4, 4, 4, 8, 12, 11, 9, 13, 9]
+
+        got = hs.heapify(unheaped)
+
+        self.assertEqual(got, expected,
+        "heapify() failed.\
+            \n\tExpected:\t{}\
+            \n\tGot:\t\t{}".format(expected, got))
+
+    def test_bubble_down(self):
+        """
+        Test to ensure proper function of bubble_down().
+        """
+
+        # Remember, 0 is a place-holder and heap is a 1-based index.
+        heap = [0, 8, 2, 3, 4, 5, 6, 7]
+        expected = [0, 2, 4, 3, 8, 5, 6, 7]
+
+        hs.bubble_down(1, heap)
+
+        self.assertEqual(heap, expected, 
+        "bubble_down() failed.\
+            \n\tExpected:\t{}\
+            \n\tGot:\t\t{}".format(expected, heap))
+
+    def test_extract_min(self):
+        """
+        Check on extract_min results.
+        """
+
+        # Remember, 0 is a place-holder and heap is a 1-based index.
+        heap = [0]
+        expected = 0
+        got, heap = hs.extract_min(heap)
+        self.assertEqual(got, expected, 
+        "extract_min() failed. Expected: {}, Got: {}".format(expected, got))
+        
+        heap = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        expected = [1, 2, 3, 4, 5, 6, 7, 8]
+
+        got = []
+        while len(heap) > 1:
+            min_value, heap = hs.extract_min(heap)
+            got.append(min_value)
+
+        self.assertEqual(got, expected,
+        "extract_min() failed.\
+            \n\tExpected:\t{}\
+            \n\tGot:\t\t{}".format(expected, got))
 
 if __name__ == "__main__":
     unittest.main()
