a260ec802730af4021fd709394b0804939e0022e
jeffvswanson <jeff.v.swanson@gmail.com>
2019-07-08 20:05:45 -0600

    Add class based heap

--- Stanford/ProgrammingQuestion6_HeapsSearchTreesAndHashTables/Question2a_HeapMedianMaintenance/heap_median_maintenance.py
+++ Stanford/ProgrammingQuestion6_HeapsSearchTreesAndHashTables/Question2a_HeapMedianMaintenance/heap_median_maintenance.py
@@ -21,7 +21,148 @@ OPTIONAL EXERCISE: Compare the performance achieved by heap-based and
 search-tree-based implementations of the algorithm.
 """
 
+class Node:
+    """Node represents where a value goes in the heap"""
+
+    def __init__(self, value):
+        self.node = value
+
+    def get_parent_index(self, child_index):
+        """
+        Get the index of the given node's parent given the index 
+        of the child node.
+        """
+
+        # The root of the tree is at index position 1 
+        # and can have no parent
+        if child_index == 1:
+            return 0
+        return child_index // 2
+
+    def get_left_child_index(self, parent_index, heap):
+        """
+        Get the index of the left child given the parent
+        node's index.
+        """
+
+        # Remember this is a 1-base index.
+        if parent_index * 2 >= len(heap):
+            # There is no left-child
+            return 0
+
+        return parent_index * 2
+
+    def get_right_child_index(self, parent_index, heap):
+        """
+        Get the index of the right childe given the 
+        parent node's index.
+        """
+
+        # Remember, this is a 1-base index.
+        if parent_index * 2 + 1 >= len(heap):
+            # There is no right child
+            return 0
+
+        return parent_index * 2 + 1
+
+class Heap:
+    """Heap represents the heap data structure."""
+
+    def __init__(self, *args):
+
+        # Array to start the heap data structure. 
+        # Actual nodes start at heap[1] as a heap is a 1-based index.
+        # The 0 is a placeholder.
+        self.heap = [0]
+        for node in args:
+            insert(node)
+
+    def insert(self, node):
+        """Inserts a node into the heap."""
+
+        # The first element, whatever it is, gets put in the root node
+        # position.
+        if len(self.heap) < 2:
+            self.heap.append(node)
+            return
+        
+        self.heap.append(node)
+
+        bubble_up(len(self.heap)-1)
+
+        return
+    
+    def bubble_up(self, child_index):
+        """
+        Push a child node up through the heap to maintain the heap property.
+        """
+
+        parent_index = Node.get_parent_index(child_index, self.heap)
+        # The node in question is the root node
+        if parent_index == 0:
+            return
+
+        if self.heap[child_index] < self.heap[parent_index]:
+            self.heap[child_index], self.heap[parent_index] = \
+                self.heap[parent_index], self.heap[child_index]
+            bubble_up(parent_index)
+
+        return
+
+    def extract_min(self):
+        """Returns the minimum value of the heap, the root."""
+
+        # Check if the heap only has the 0-element.
+        if len(self.heap) < 2:
+            return self.heap[0], self.heap
+
+        root = self.heap[1]
+        # Swap the last index position into the root node
+        self.heap[1] = self.heap[-1]
+        # Pare down the list since the last node became the root
+        del self.heap[-1]
+
+        if len(self.heap) > 1:
+            bubble_down(1)
+
+        return root
+
+    def bubble_down(self, parent_index):
+        """Moves a misplaced node into its appropriate position."""
+
+        min_val = self.heap[parent_index]
+        min_index = Node.get_left_child_index(parent_index, self.heap)
+
+        # The parent node has no children
+        if min_index == 0:
+            return
+
+        # Find the smaller of the two children
+        right_child_index = Node.get_right_child_index(parent_index, self.heap)
+        if right_child_index == 0:
+            right_child_index = min_index
+        
+        if self.heap[right_child_index] < self.heap[min_index]:
+            min_index = right_child_index
+
+        if self.heap[min_index] < min_val:
+            min_val = self.heap[min_index]
+
+        if min_val != self.heap[parent_index]:
+            self.heap[parent_index], self.heap[min_index] = self.heap[min_index], self.heap[parent_index]
+            bubble_down(min_index)
+
 def main():
+
+    min_heap = Heap()
+    max_heap = Heap()
+
+    print(type(min_heap))
+    print(len(max_heap.heap))
+
+    max_heap.insert(1)
+
+    print(len(max_heap.heap))
 
 if __name__ == "__main__":
     main()
