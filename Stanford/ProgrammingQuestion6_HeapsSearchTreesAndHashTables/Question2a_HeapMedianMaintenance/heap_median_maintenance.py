99a2e6a8f266268510de1e3ea4ab02a18af2bce0
jeffvswanson <jeff.v.swanson@gmail.com>
2019-07-10 17:18:51 -0600

    Working, but not tested

--- Stanford/ProgrammingQuestion6_HeapsSearchTreesAndHashTables/Question2a_HeapMedianMaintenance/heap_median_maintenance.py
+++ Stanford/ProgrammingQuestion6_HeapsSearchTreesAndHashTables/Question2a_HeapMedianMaintenance/heap_median_maintenance.py
@@ -27,6 +27,115 @@ class Node:
     def __init__(self, value):
         self.node = value
 
+class Heap:
+    """
+    Heap represents the heap data structure.
+    
+    Default kind is a min-heap, unless 'max' denoted."""
+
+    def __init__(self, kind="min", *args):
+
+        # Array to start the heap data structure. 
+        # Actual nodes start at heap[1] as a heap is a 1-based index.
+        # The 0 is a placeholder.
+        self.heap = [0]
+        self.kind = kind
+        for node in args:
+            insert(node)
+
+    def insert(self, node):
+        """Inserts a node into the heap."""
+
+        # The first element, whatever it is, gets put in the root node
+        # position.
+        if len(self.heap) < 2:
+            if self.kind == "max":
+                self.heap.append(-node)
+            else: 
+                self.heap.append(node)
+            return
+        
+        if self.kind == "max":
+            self.heap.append(-node)
+        else:
+            self.heap.append(node)
+
+        self.bubble_up(len(self.heap)-1)
+
+        return
+    
+    def bubble_up(self, child_index):
+        """
+        Push a child node up through the heap to maintain the heap property.
+        """
+
+        parent_index = self.get_parent_index(child_index)
+        # The node in question is the root node
+        if parent_index == 0:
+            return
+
+        if self.heap[child_index] < self.heap[parent_index]:
+            self.heap[child_index], self.heap[parent_index] = \
+                self.heap[parent_index], self.heap[child_index]
+            self.bubble_up(parent_index)
+
+        return
+
+    def extract_root(self):
+        """
+        Extracts and returns the max or min root value of the heap, 
+        dependent on heap type.
+
+        Returns True or False if there is a root node to return. That
+        is, a heap without a root node (0-element heap) has a return
+        value of a 0 and a False.
+        """
+
+        # Check if the heap only has the 0-element.
+        # False return because there is no root element.
+        if self.length() < 1:
+            return self.heap[0], False
+
+        if self.kind == "max":
+            root = -self.heap[1]
+        else:
+            root = self.heap[1]
+
+        # Swap the last index position into the root node
+        self.heap[1] = self.heap[-1]
+        # Pare down the list since the last node became the root
+        del self.heap[-1]
+
+        if self.length() > 1:
+            self.bubble_down(1)
+            
+        return root, True
+
+    def bubble_down(self, parent_index):
+        """Moves a misplaced node into its appropriate position."""
+
+        min_val = self.heap[parent_index]
+        min_index = self.get_left_child_index(parent_index)
+
+        # The parent node has no children
+        if min_index == 0:
+            return
+
+        # Find the smaller of the two children
+        right_child_index = self.get_right_child_index(parent_index)
+        if right_child_index == 0:
+            right_child_index = min_index
+        
+        if self.heap[right_child_index] < self.heap[min_index]:
+            min_index = right_child_index
+
+        if self.heap[min_index] < min_val:
+            min_val = self.heap[min_index]
+
+        if min_val != self.heap[parent_index]:
+            self.heap[parent_index], self.heap[min_index] = self.heap[min_index], self.heap[parent_index]
+            self.bubble_down(min_index)
+
     def get_parent_index(self, child_index):
         """
         Get the index of the given node's parent given the index 
@@ -39,130 +148,137 @@ class Node:
             return 0
         return child_index // 2
 
-    def get_left_child_index(self, parent_index, heap):
+    def get_left_child_index(self, parent_index):
         """
         Get the index of the left child given the parent
         node's index.
         """
 
         # Remember this is a 1-base index.
-        if parent_index * 2 >= len(heap):
+        if parent_index * 2 >= len(self.heap):
             # There is no left-child
             return 0
 
         return parent_index * 2
 
-    def get_right_child_index(self, parent_index, heap):
+    def get_right_child_index(self, parent_index):
         """
-        Get the index of the right childe given the 
+        Get the index of the right child given the 
         parent node's index.
         """
 
         # Remember, this is a 1-base index.
-        if parent_index * 2 + 1 >= len(heap):
+        if parent_index * 2 + 1 >= len(self.heap):
             # There is no right child
             return 0
 
         return parent_index * 2 + 1
 
-class Heap:
-    """Heap represents the heap data structure."""
-
-    def __init__(self, *args):
-
-        # Array to start the heap data structure. 
-        # Actual nodes start at heap[1] as a heap is a 1-based index.
-        # The 0 is a placeholder.
-        self.heap = [0]
-        for node in args:
-            insert(node)
-
-    def insert(self, node):
-        """Inserts a node into the heap."""
-
-        # The first element, whatever it is, gets put in the root node
-        # position.
-        if len(self.heap) < 2:
-            self.heap.append(node)
-            return
-        
-        self.heap.append(node)
-
-        bubble_up(len(self.heap)-1)
-
-        return
-    
-    def bubble_up(self, child_index):
+    def peek(self):
         """
-        Push a child node up through the heap to maintain the heap property.
+        Returns the root of the heap without extracting the root.
+
+        Returns True or False if there is a root node to return. That
+        is, a heap without a root node (0-element heap) has a return
+        value of a 0 and a False.
         """
-
-        parent_index = Node.get_parent_index(child_index, self.heap)
-        # The node in question is the root node
-        if parent_index == 0:
-            return
-
-        if self.heap[child_index] < self.heap[parent_index]:
-            self.heap[child_index], self.heap[parent_index] = \
-                self.heap[parent_index], self.heap[child_index]
-            bubble_up(parent_index)
-
-        return
-
-    def extract_min(self):
-        """Returns the minimum value of the heap, the root."""
 
         # Check if the heap only has the 0-element.
-        if len(self.heap) < 2:
-            return self.heap[0], self.heap
+        # False return because there is no root element.
+        if self.length() < 1:
+            return self.heap[0], False
 
-        root = self.heap[1]
-        # Swap the last index position into the root node
-        self.heap[1] = self.heap[-1]
-        # Pare down the list since the last node became the root
-        del self.heap[-1]
+        if self.kind == "max":
+            root = -self.heap[1]
+        else:
+            root = self.heap[1]
+            
+        return root, True
 
-        if len(self.heap) > 1:
-            bubble_down(1)
+    def length(self):
+        """
+        Returns the length of the 1-based index heap.
+        """
 
-        return root
+        return len(self.heap) - 1
 
-    def bubble_down(self, parent_index):
-        """Moves a misplaced node into its appropriate position."""
-
-        min_val = self.heap[parent_index]
-        min_index = Node.get_left_child_index(parent_index, self.heap)
-
-        # The parent node has no children
-        if min_index == 0:
-            return
-
-        # Find the smaller of the two children
-        right_child_index = Node.get_right_child_index(parent_index, self.heap)
-        if right_child_index == 0:
-            right_child_index = min_index
-        
-        if self.heap[right_child_index] < self.heap[min_index]:
-            min_index = right_child_index
-
-        if self.heap[min_index] < min_val:
-            min_val = self.heap[min_index]
-
-        if min_val != self.heap[parent_index]:
-            self.heap[parent_index], self.heap[min_index] = self.heap[min_index], self.heap[parent_index]
-            bubble_down(min_index)
 
 def main():
 
-    min_heap = Heap()
-    max_heap = Heap()
+    # Used to store highest half of elements
+    high_half_heap = Heap()
+    # Used to store lowest half of elements
+    low_half_heap = Heap("max")
 
-    print(type(min_heap))
-    print(len(max_heap.heap))
+    # even_or_odd tracks whether the count of elements inserted into
+    # the heap are even or odd. This should only vary between 0 to 2
+    # we don't want the number getting arbitrarily large as the heap
+    # grows.
+    even_or_odd = 0
+    median, median_total = 0, 0
+    count = 0
 
-    max_heap.insert(1)
+    with open("Median.txt") as f:
+        # Exclude the newline character, \n
+        for line in f:
+            x = int(line.rstrip('\n'))
+            count += 1
 
-    print(len(max_heap.heap))
+            # Put x into the correct heap
+            # If x > median, place x in high_half_heap
+            if x > median:
+                high_half_heap.insert(x)
+            else:
+                low_half_heap.insert(x)
+            
+            # Check if heaps need to be rebalanced
+            rebalance(high_half_heap, low_half_heap)
+
+            # Calculate the median given the new x
+            median = find_median(high_half_heap, low_half_heap, count)
+
+            # Add the median to the median_total
+            median_total += median
+            print("{}.\tmedian = {}\tmedian_total = {}".format(count, median, median_total))
+            
+    # Once the file is exhausted return_value = median_total%10000
+    print("The last four digits of the sums of the medians is {}".format(median_total%10000))
+
+def rebalance(h_high, h_low):
+    """
+    rebalance prevents one heap from hoarding all the values by 
+    keeping the difference in the number of items between each 
+    heap less than 2.
+    """
+
+    if abs(h_high.length() - h_low.length()) > 1:
+                if h_high.length() > h_low.length():
+                    root, prs = h_high.extract_root()
+                    if prs:
+                        h_low.insert(root)
+                else:
+                    root, prs = h_low.extract_root()
+                    if prs:
+                        h_high.insert(root)
+
+def find_median(h_high, h_low, count):
+    """
+    find_median determines the median value between the two heaps.
+    """
+
+    # r is the median's index
+    r = 0
+    if count%2 == 0:
+        r = count//2
+    else:
+        r = (count + 1)//2
+        
+    if r > h_low.length():
+        median, _ = h_high.peek()
+    else:
+        median, _ = h_low.peek()
+        
+    return median
 
 if __name__ == "__main__":
     main()
