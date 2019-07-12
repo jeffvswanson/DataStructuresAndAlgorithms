68f3b82b1720c775a031709a5e8a4ffae7a7b94c
jeffvswanson <jeff.v.swanson@gmail.com>
2019-07-11 20:26:21 -0600

    Work on developing tests

--- Stanford/ProgrammingQuestion6_HeapsSearchTreesAndHashTables/Question2a_HeapMedianMaintenance/test_heap_median_maintenance.py
+++ Stanford/ProgrammingQuestion6_HeapsSearchTreesAndHashTables/Question2a_HeapMedianMaintenance/test_heap_median_maintenance.py
@@ -1,10 +1,24 @@
 # test_heap_median_maintenance.py
 
 import unittest
+
 import heap_median_maintenance as hm
 
 class TestHeapMedianMaintenance(unittest.TestCase):
+    
+    def test_rebalance(self):
+        pass
 
+    def test_find_median(self):
+        pass
+
+    def test_find_last_four(self):
+        
+        test_total = 89178765
+        expected = 8765
+        got = hm.find_last_four(test_total)
+
+        self.assertEqual(expected, got, "find_last_four() failure.")
 
 if __name__ == "__main__":
     unittest.main()
