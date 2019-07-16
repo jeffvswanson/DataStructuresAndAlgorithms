79e832b747d073578ff99e2e15cdbeab74190a74
jeffvswanson <jeff.v.swanson@gmail.com>
2019-07-16 07:58:49 -0600

    Progress commit of program and tests

--- Stanford/ProgrammingQuestion6_HeapsSearchTreesAndHashTables/Question2a_HeapMedianMaintenance/main.go
+++ Stanford/ProgrammingQuestion6_HeapsSearchTreesAndHashTables/Question2a_HeapMedianMaintenance/main.go
@@ -21,8 +21,243 @@ search-tree-based implementations of the algorithm.
 */
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+type heap struct {
+	Type string
+	xi   []int
+}
 
 func main() {
-	fmt.Println("Hello")
+
+	f, err := os.Open("Median.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	// highValHeap stores the higher half of elements in the set in a min-heap.
+	highValHeap := heap{Type: "min"}
+
+	// lowValHeap storse the lower half of elements in the set in a max-heap.
+	lowValHeap := heap{Type: "max"}
+
+	median, medianTotal := 0, 0
+
+	s := bufio.NewScanner(f)
+	s.Split(bufio.ScanLines)
+
+	for s.Scan() {
+		x, err := strconv.Atoi(s.Text())
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// Put x into the correct heap.
+		// If x > median, place x in highValHeap.
+		if x > median {
+			highValHeap.insert(x)
+		} else {
+			lowValHeap.insert(x)
+		}
+
+		// Check if heaps need to be rebalanced
+		rebalance(&lowValHeap, &highValHeap)
+
+		// Calculate the median given the new x
+		median = findMedian(&lowValHeap, &highValHeap)
+
+		// Add the median to the medianTotal.
+		medianTotal += median
+	}
+
+	// Once the file is exhausted returnValue = medianTotal%10000.
+	output := findLastFour(medianTotal)
+	fmt.Printf("The last four digits of the sums of the medians is %d\n", output)
+}
+
+// findLastFour returns the last four numbers of an integer.
+func findLastFour(x int) int {
+	return x % 10000
+}
+
+// findMedian determines the median value between the two heaps.
+func findMedian(lowValHeap, highValHeap *heap) int {
+
+	count := lowValHeap.hLen() + highValHeap.hLen()
+	// i is the median's index
+	i, median := 0, 0
+
+	if count%2 == 0 {
+		i = count / 2
+	} else {
+		i = (count + 1) / 2
+	}
+
+	if i > lowValHeap.hLen() {
+		median, _ = highValHeap.peek()
+	} else {
+		median, _ = lowValHeap.peek()
+	}
+
+	return median
+}
+
+// rebalance ensures the two input heaps remain within one element
+// of one another.
+func rebalance(h1, h2 *heap) {
+
+	if math.Abs(float64(h1.hLen())-float64(h2.hLen())) > 1 {
+		if h1.hLen() > h2.hLen() {
+			root, prs := h1.extractRoot()
+			if prs {
+				h2.insert(root)
+			}
+		} else {
+			root, prs := h2.extractRoot()
+			if prs {
+				h1.insert(root)
+			}
+		}
+		rebalance(h1, h2)
+	}
+}
+
+// hLen returns the length of a slice with a 1-based index.
+func (h *heap) hLen() int {
+	return len(h.xi) - 1
+}
+
+// insert the given key, k, into the heap.
+func (h *heap) insert(k int) {
+
+	// The max heap type has the insertion value negated to allow a
+	// min heap structure that will organize by max values.
+	// The inserted value gets converted back to its correct value
+	// on a peek or extraction.
+	if h.Type == "max" {
+		h.xi = append(h.xi, -k)
+	} else {
+		h.xi = append(h.xi, k)
+	}
+	h.bubbleUp(h.hLen())
+}
+
+// extractRoot returns the minimum value of the heap, the root.
+func (h *heap) extractRoot() (int, bool) {
+	// Check if the heap only has the 0-element
+	if h.hLen() == 0 {
+		return 0, false
+	}
+
+	var root int
+	if h.Type == "max" {
+		root = -h.xi[1]
+	} else {
+		root = h.xi[1]
+	}
+	// Swap the last index position into the root node.
+	h.xi[1] = h.xi[h.hLen()]
+	// Pare down the list since the last node becam the root
+	h.xi = h.xi[:h.hLen()-1]
+
+	if h.hLen() > 1 {
+		h.bubbleDown(1)
+	}
+	return root, true
+}
+
+// bubbleUp moves the key at index, i, into position in the heap.
+func (h *heap) bubbleUp(i int) {
+	p, ok := parentIndex(i)
+	if !ok {
+		return // k is the root node
+	}
+	if h.xi[p] > h.xi[i] {
+		h.swap(i, p)
+		h.bubbleUp(p)
+	}
+}
+
+// bubbleDown moves a misplaced node into its appropriate position to
+// maintain the heap property using its index.
+func (h *heap) bubbleDown(i int) {
+
+	// Find the smaller of the two values between the node at i and i's
+	// two children.
+	min := i
+	left, prs := h.leftIndex(i)
+	if prs {
+		if h.xi[i] > h.xi[left] {
+			min = left
+		}
+	}
+	r, prs := h.rightIndex(i)
+	if prs {
+		if h.xi[i] > h.xi[r] {
+			min = r
+		}
+	}
+	if min != i {
+		h.swap(i, min)
+		h.bubbleDown(min)
+	}
+
+}
+
+// peek returns the value at the root of a heap, but does not remove it.
+func (h *heap) peek() (int, bool) {
+
+	root := 0
+	// Check if the heap only has the 0-element.
+	// False return because there is no root element.
+	if h.hLen() < 1 {
+		return root, false
+	}
+
+	if h.Type == "max" {
+		root = -h.xi[1]
+	} else {
+		root = h.xi[1]
+	}
+
+	return root, true
+}
+
+// swap swaps the position of two values with one another.
+func (h *heap) swap(a, b int) {
+	h.xi[a], h.xi[b] = h.xi[b], h.xi[a]
+}
+
+// parentIndex returns the index of the parent node to the node a index i.
+func parentIndex(i int) (int, bool) {
+	// i is the root node and therefore has no parent.
+	if i < 2 {
+		return 0, false
+	}
+	return i / 2, true
+}
+
+// leftIndex returns the left child index for the parent node at index i.
+func (h *heap) leftIndex(i int) (int, bool) {
+	c := 2 * i
+	if c > h.hLen() || i == 0 {
+		return 0, false
+	}
+	return c, true
+}
+
+// rightIndex returns the right child index for the parent node at index i.
+func (h *heap) rightIndex(i int) (int, bool) {
+	c := 2*i + 1
+	if c > h.hLen() || i == 0 {
+		return 0, false
+	}
+	return c, true
 }
--- Stanford/ProgrammingQuestion6_HeapsSearchTreesAndHashTables/Question2a_HeapMedianMaintenance/main_test.go
+++ Stanford/ProgrammingQuestion6_HeapsSearchTreesAndHashTables/Question2a_HeapMedianMaintenance/main_test.go
@@ -1,1 +1,193 @@
 package main
+
+import "testing"
+
+func TestFindLastFour(t *testing.T) {
+	tests := map[string]struct {
+		input int
+		want  int
+	}{
+		"less than 10,000":    {input: 100, want: 100},
+		"equals 10000":        {input: 10000, want: 0},
+		"greater than 10,000": {input: 15156, want: 5156},
+	}
+
+	for name, cond := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := findLastFour(cond.input)
+			if got != cond.want {
+				t.Fatalf("expected: %d, got: %v\n", cond.want, got)
+			}
+		})
+	}
+}
+
+func TestParentIndex(t *testing.T) {
+	type want struct {
+		parentIndex int
+		hasParent   bool
+	}
+
+	tests := map[string]struct {
+		input int
+		want
+	}{
+		"empty heap":         {input: 0, want: want{0, false}},
+		"root has no parent": {input: 1, want: want{0, false}},
+		"node has parent":    {input: 6, want: want{3, true}},
+	}
+
+	for name, cond := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, ok := parentIndex(cond.input)
+			if got != cond.want.parentIndex || ok != cond.want.hasParent {
+				t.Fatalf("expected: %d, %t; got: %d, %t\n", cond.want.parentIndex, cond.want.hasParent, got, ok)
+			}
+		})
+	}
+}
+
+func TestHLen(t *testing.T) {
+
+	// Remember, all heaps are initialized with a 0 in the 0-index position
+	// as heaps are based on a 1-based index.
+	emptyHeap := heap{Type: "min", xi: []int{0}}
+	minHeap := heap{Type: "min", xi: []int{0, 1}}
+	// The max heap is constructed from negating input values
+	maxHeap := heap{Type: "max", xi: []int{0, -10, -9, -8, -7, -6}}
+
+	tests := map[string]struct {
+		input heap
+		want  int
+	}{
+		"empty heap":            {input: emptyHeap, want: 0},
+		"1-element heap":        {input: minHeap, want: 1},
+		"multiple element heap": {input: maxHeap, want: 5},
+	}
+
+	for name, cond := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := cond.input.hLen()
+			if got != cond.want {
+				t.Fatalf("expected: %d, got: %d\n", cond.want, got)
+			}
+		})
+	}
+}
+
+func TestSwap(t *testing.T) {
+
+	testHeap := heap{Type: "min", xi: []int{0, 1, 2}}
+	want := []int{0, 2, 1}
+
+	testHeap.swap(1, 2)
+	for i, v := range testHeap.xi {
+		if v != want[i] {
+			t.Fatalf("at index position %d on swap() - expected: %d, got %d\n", i, want[i], v)
+		}
+	}
+}
+
+func TestPeek(t *testing.T) {
+
+	// Remember, all heaps are initialized with a 0 in the 0-index position
+	// as heaps are based on a 1-based index.
+	emptyHeap := heap{Type: "min", xi: []int{0}}
+	minHeap := heap{Type: "min", xi: []int{0, 1, 2, 3, 4, 5}}
+	// The max heap is constructed from negating input values.
+	maxHeap := heap{Type: "max", xi: []int{0, -10, -9, -8, -7, -6}}
+
+	type want struct {
+		root    int
+		hasRoot bool
+	}
+
+	tests := map[string]struct {
+		input heap
+		want
+	}{
+		"empty heap": {emptyHeap, want{0, false}},
+		"min heap":   {minHeap, want{1, true}},
+		"max heap":   {maxHeap, want{10, true}},
+	}
+
+	for name, cond := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, prs := cond.input.peek()
+			if got != cond.want.root || prs != cond.want.hasRoot {
+				t.Fatalf("expected: %d, %t; got: %d, %t\n", cond.want.root, cond.want.hasRoot, got, prs)
+			}
+		})
+	}
+}
+
+func TestFindMedian(t *testing.T) {
+
+	// One item in min-heap 0 in max-heap
+	oneMinHeap := []heap{
+		heap{Type: "min", xi: []int{0, 6}},
+		heap{Type: "max", xi: []int{0}},
+	}
+	// One item in max-heap 0 in min-heap
+	oneMaxHeap := []heap{
+		heap{Type: "min", xi: []int{0}},
+		heap{Type: "max", xi: []int{0, -10}},
+	}
+	// Even number of items in both min and max-heaps
+	evenHeaps := []heap{
+		heap{Type: "min", xi: []int{0, 6, 7}},
+		heap{Type: "max", xi: []int{0, -10, -9}},
+	}
+
+	tests := map[string]struct {
+		input []heap
+		want  int
+	}{
+		"1-value min-heap": {oneMinHeap, 6},
+		"1-value max-heap": {oneMaxHeap, 10},
+		"even heaps":       {evenHeaps, 6},
+	}
+
+	for name, cond := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := findMedian(&cond.input[0], &cond.input[1])
+			if got != cond.want {
+				t.Fatalf("expected: %d, got: %d\n", cond.want, got)
+			}
+		})
+	}
+}
+
+func TestBubbleUp(t *testing.T) {
+	minHeap := heap{Type: "min", xi: []int{0, 10, 9, 8, 7, 6}}
+	maxHeap := heap{Type: "max", xi: []int{0, -1, -2, -3, -4, -5}}
+
+	tests := map[string]struct {
+		input heap
+		want  heap
+	}{
+		"min-heap bubble up": {minHeap, heap{"min", []int{0, 6, 10, 8, 7, 9}}},
+		"max-heap bubble up": {maxHeap, heap{"max", []int{0, -5, -1, -3, -4, -2}}},
+	}
+
+	for name, cond := range tests {
+		t.Run(name, func(t *testing.T) {
+			cond.input.bubbleUp(5)
+			for i, got := range cond.input.xi {
+				if got != cond.want.xi[i] {
+					t.Fatalf("at index position %d after bubbleUp expected: %d, got: %d\n", i, cond.want.xi[i], got)
+				}
+			}
+		})
+	}
+}
+
+// func TestInsert(t *testing.T) {}
+
+// func TestLeftIndex(t *testing.T) {}
+
+// func TestRightIndex(t *testing.T) {}
+
+// func TestBubbleDown(t *testing.T) {}
+
+// func TestExtractRoot(t *testing.T) {}
